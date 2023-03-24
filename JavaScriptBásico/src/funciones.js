9f406e9cffe59b98b8dfcb625b55d9ae8f13d0ab
ImKaze7 <josefernandoramosalania3@gmail.com>
2023-03-23 20:36:43 -0500

    funciones js

---
+++ JavaScriptBásico/src/funciones.js
@@ -0,0 +1,32 @@
+//declarativas
+function miFuncion(){
+    return 3;
+}
+
+miFuncion();
+//expresion
+var miFuncion = function(){
+    return a + b;
+}
+
+miFuncion();
+
+/*
+
+
+function sumar(a,b){
+    var resultado = a + b;
+    return resultado;
+
+    sumar(1,2);
+
+
+     function sumar(a,b){
+    return a + b;
+
+    sumar(1,2);
+
+}
+
+
+}*/
