9d8662cbd391fd57986ba2ce6568e6b8bb583ac2
ImKaze7 <josefernandoramosalania3@gmail.com>
2023-03-23 20:10:02 -0500

    variables

---
+++ JavaScriptBásico/src/variables.js
@@ -0,0 +1,14 @@
+var nombre = "Jose";
+//declarar
+var edad;
+//inicializar
+edad = 30;
+
+edad
+
+var elementos = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
