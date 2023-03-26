df7b4d5f09d659785a9f7088144b9e41fc4157fa
ImKaze7 <josefernandoramosalania3@gmail.com>
2023-03-26 18:23:03 -0500

    hoisting

---
+++ JavaScriptBásico/src/hoisting.js
@@ -0,0 +1,17 @@
+console.log(miNombre);
+
+miNombre = "Diego";
+
+hey();
+    function hey(){
+        console.log("Hola " + miNombre);
+
+    }
+
+
+    /*hey();
+    function hey(){
+        console.log("Hola " + miNombre);
+
+    }
+var miNombre = "Diego";*/
