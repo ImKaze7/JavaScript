c356cb132a7461d9e9f5a7ad10a3a417c2f8fbbf
ImKaze7 <josefernandoramosalania3@gmail.com>
2023-03-26 18:44:28 -0500

    coerción

---
+++ JavaScriptBásico/src/coerción.js
@@ -0,0 +1,25 @@
+var a= 4 + "7";
+
+var b = 4* "7";  
+//
+var a = 20;
+var b = a + "";
+
+console.log(b);
+
+20
+//
+typeof b
+'string'
+//
+var c = String(a);
+//
+typeof c
+'string'
+console.log(c);
+20
+//
+var d = Number(c);
+//
+typeof d
+'number'
