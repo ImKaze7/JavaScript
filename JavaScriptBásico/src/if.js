175079a75ea83246b0f5f6079653a2f119a0a21a
ImKaze7 <josefernandoramosalania3@gmail.com>
2023-03-26 19:39:15 -0500

    if else

---
+++ JavaScriptBásico/src/if.js
@@ -0,0 +1,21 @@
+if(true){
+    console.log("Hola");
+}else{
+    console.log("soy falso")
+}
+//
+var edad = 18;
+if(edad === 18) {
+    console.log("Puedes votar, será tu 1ra votación");
+
+}else if (edad > 18){
+    console.log("Puedes votar de nuevo");
+}else{
+    console.log("Aún no puedes votar");
+}
+//
+var numero = 1;
+
+var resultado = numero === 1 ? "Sí, soy un uno" : "No, no soy uno"; 
+
+console.log(resultado)
