235eb96608bda8a57f315bd8ab6b92dc04ed26ee
ImKaze7 <josefernandoramosalania3@gmail.com>
2023-03-30 20:18:00 -0500

    Arrays

---
+++ JavaScriptBásico/src/arrays.js
@@ -0,0 +1,16 @@
+var frutas = ["Manzana", "Plátano", "Cereza", "Fresa"];
+console.log(frutas);
+
+//
+console.log(frutas.length);
+//
+console.log(frutas[0]);
+//
+console.log(frutas[2]);
+
+//PARA AGREGAR
+
+var masFrutas = frutas.push("Uvas");
+
+//para quitar
+var ultimo = frutas.pop("Uvas");
